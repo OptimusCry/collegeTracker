1094149ab0b37eb71313c128dba0642c800325a8
OptimusCry <kayla.coykendall@yahoo.com>
2025-03-11 15:42:55 -0400

    Revert "Study Timer"
    This reverts commit e030f2e352af2fbff789c12c9025f2f369393efa.

--- College Tracker/src/college/tracker/CollegeTracker.java
+++ College Tracker/src/college/tracker/CollegeTracker.java
@@ -3,10 +3,7 @@ package college.tracker;
 
 import static college.tracker.database.connect.connect;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.input.KeyEvent;
 
 /**
  *
@@ -15,28 +12,19 @@ import javafx.scene.input.KeyEvent;
 public class CollegeTracker extends Application {
 
     /**
-     * @param primaryStage
+     * @param stage
+     * @param args the command line arguments
      * @throws java.lang.Exception
      */
     
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
         connect();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/college/tracker/FXML.fxml"));
-        
-        Scene scene = new Scene(loader.load(), 1000, 800);
-        
-        primaryStage.setTitle("College Tracker");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-           
     }
     
     public static void main(String[] args) {
         launch(args);
         
     }
-    
-    
     
 }
--- College Tracker/src/college/tracker/FXMLController.java
+++ College Tracker/src/college/tracker/FXMLController.java
@@ -1,17 +1,12 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package college.tracker;
 
 import java.net.URL;
-import java.time.Duration;
 import java.util.ResourceBundle;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.KeyCode;
 
 /**
  * FXML Controller class
@@ -20,156 +15,12 @@ import javafx.scene.input.KeyCode;
  */
 public class FXMLController implements Initializable {
 
-    @FXML
-    private TextField timer_input;
-    
-    @FXML
-    private TextField timer_output;
-    
-    private myTimer studyTimer;
-
     /**
      * Initializes the controller class.
-     * @param url
-     * @param rb
      */
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        studyTimer = new myTimer(() -> updateTimer());
-    }
+        // TODO
+    }    
     
-   // start of code for the study timer
-    
-    public void handleKeyPress(KeyEvent event) {
-        if (event.getCode() == KeyCode.ENTER) {
-            setTime();
-        }
-    }
-    
-    public void handleTimeButton(ActionEvent event) {
-        // getting the button that was clicked by the user
-        Button sourceButton = (Button) event.getSource();
-        String time = sourceButton.getText();
-        
-        // converting the string to an int 
-        int minutes = Integer.parseInt(time);
-        Duration duration = Duration.ofMinutes(minutes);
-        
-        // calling the formatting function
-        String formattedTime = formatDuration(duration);
-        
-        studyTimer.stopTimer();
-        
-        // displays the time in the timer_ouput
-        timer_output.setText(formattedTime);
-        
-        studyTimer.startTimer((int) duration.getSeconds());
-    }
-    
-    public void handleStartButton(ActionEvent event) {
-        int remainingTime = studyTimer.getRemainingTime();
-        studyTimer.startTimer(remainingTime);
-    }
-    
-    public void handleStopButton(ActionEvent event) {
-        studyTimer.stopTimer();
-        setRemainingTime();
-    }
-    
-    public void handleResetButton(ActionEvent event) {
-        // stop the timer
-        studyTimer.stopTimer();
-        
-        // Reset the text input and output
-        timer_output.setText("00:00");
-        timer_input.clear();
-    }
-    
-    private String formatDuration(Duration duration) {
-        long hours = duration.toHours();
-        long minutes = duration.toMinutes() - hours * 60;
-        var seconds = duration.getSeconds() - minutes * 60 - hours * 3600;
-        
-        if (hours > 0 ) {
-            return String.format("%d:%02d:%02d", hours, minutes, seconds);
-        } else if (minutes > 0 && hours == 0) {
-            return String.format("%d:%02d", minutes, seconds);
-        } else {
-            return String.format("%d", seconds);
-        }
-        
-    }
-    
-    // this is for when users input their own time
-    private void setTime() {
-        String desiredTime = timer_input.getText();
-        
-        // checking the entered minutes and seconds since we want 00:00 format
-        
-        if (desiredTime.contains(":")) {
-            String[] timeParts = desiredTime.split(":");
-            
-            // this checks the HH:MM:SS format
-            if (timeParts.length == 3) {
-                try {
-                    
-                    // getting the parts of the inputted time and assigning them to their respective variables
-                    int seconds = Integer.parseInt(timeParts[2]);
-                    int minutes = Integer.parseInt(timeParts[1]);
-                    int hours = Integer.parseInt(timeParts[0]);
-                    
-                    int totalSeconds = hours * 3600 + minutes * 60 + seconds;
-                    studyTimer.startTimer(totalSeconds);
-                } catch (NumberFormatException e ) {
-                    timer_output.setText("Invalid time format, use HH:MM:SS");
-                } 
-                
-            // This checks the MM:SS format    
-            } else if (timeParts.length == 2) {
-                
-                try {
-                    int seconds = Integer.parseInt(timeParts[1]);
-                    int minutes = Integer.parseInt(timeParts[0]);
-                    int totalSeconds = minutes * 60 + seconds;
-                    studyTimer.startTimer(totalSeconds);
-                } catch (NumberFormatException e ) {
-                    timer_output.setText("Invalid time format, use MM:SS");
-                } 
-            } 
-        
-        // This just checks for seconds 
-        } else {
-            
-            try {
-                int totalSeconds = Integer.parseInt(desiredTime);
-                studyTimer.startTimer(totalSeconds);
-                
-            } catch (NumberFormatException e ) {
-                timer_output.setText("Invalid input. Enter HH:MM:SS, MM:SS, or the amount in seconds ");
-            }
-        }
-        
-        // clear the user input and unfocus it 
-        timer_input.clear();
-        timer_output.requestFocus();
-                
-    }
-    
-    public void updateTimer() {
-        Platform.runLater(() -> {
-            int remainingTime = studyTimer.getRemainingTime();
-            Duration duration = Duration.ofSeconds(remainingTime);
-            timer_output.setText(formatDuration(duration));
-        });
-    }
-    
-    private void setRemainingTime() {
-        int remainingTime = studyTimer.getRemainingTime();
-        Duration duration = Duration.ofSeconds(remainingTime);
-        timer_output.setText(formatDuration(duration));
-    }
-    
-    // end of code for study timer
-   
 }
